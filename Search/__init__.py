2332c9edef869dd0f57cf86d4ec0aa9a2813058d
Alexey Polazin <alexpolazin@gmail.com>
2023-06-03 21:28:46 +0400

    Add __init__ for some dir

---
+++ Search/__init__.py
@@ -0,0 +1,5 @@
+from binary_search import binary_search  # noqa
+from eratosthenes_prime_search import eratosthenes_prime_search  # noqa
+from is_prime import is_prime  # noqa
+from linear_prime_search import linear_prime_search  # noqa
+from linear_search import linear_search  # noqa
